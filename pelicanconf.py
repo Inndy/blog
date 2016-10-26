e597ba6c25129e7c59296016ef509216ef70ead6
Inndy <inndy.tw@gmail.com>
2016-10-26 15:09:16 +0800

    Add plugins and config social links

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,9 +2,14 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})-(?P<slug>.*)'
+
 AUTHOR = 'Inndy'
 SITENAME = 'Tech Rain'
 SITEURL = ''
+
+SUB_TITLE = '技術需要雨水滋潤才能成長茁壯'
+BIO = "It's Inndy speaking."
 
 PATH = 'content'
 
@@ -19,20 +24,26 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('github', 'https://github.com/Inndy'),
+    ('twitter', 'https://twitter.com/Inndy_tw'),
+)
 
 DEFAULT_PAGINATION = 15
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+PLUGIN_PATH = '../pelican-plugins/'
+PLUGINS = [
+    'sitemap',
+]
+
+SITEMAP = {
+    'format': 'xml',
+    'exclude': ['tag/', 'category/']
+}
 
 THEME = '../pelican-hyde/'
 PROFILE_PIC = 'https://gravatar.com/avatar/26cdb92aafb2b2e6c8972edebda4bd17?s=360'
